b0cdba37df9df9c4f4aa028d7a4d62049bbea0c5
Kaung Htet Aung <kaung.h.ag@gmail.com>
2022-04-23 22:42:17 +0630

    clean unused files

--- src/app/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import alertReducer from "../reducers/alert";
-
-export const store = configureStore({
-  reducer: {
-    alert: alertReducer,
-  },
-});
